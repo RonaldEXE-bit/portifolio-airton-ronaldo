d7fb579a42211d326ad924857dd9d31599a49170
Airton Ronaldo <Vilpandocesar7@gmail.com>
2025-11-03 22:17:54 -0300

    Em processo

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
-import Image from "next/image";
+"use client";
+
+import "./globals.css";
 
 export default function Home() {
   return (
-  <div>
-    <h1>
-      Airton Ronaldo Ronaldo
-    </h1>
-    <h3>Next.js</h3>
-  </div>
+    <>
+      <div className="video-background">
+        <video autoPlay muted loop playsInline>
+          <source src="/assets/background.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <div className="content-overlay">
+        <h1>PORTFÓLIO KRONOS – CESAR</h1>
+        <p>Interface em construção…</p>
+      </div>
+    </>
   );
 }
